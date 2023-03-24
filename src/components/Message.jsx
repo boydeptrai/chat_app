83a09dae1bef0b036ab6c9e9a769b5b5bf390721
boydeptrai <langthamht047@gmail.com>
2023-03-24 14:18:45 +0700

    send a real-time chat message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,23 +1,48 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
-import { ChatContext } from '../context/ChatContext';
+import React, { useContext, useEffect } from "react";
+import { useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = ({message}) => {
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
 
-  const {currentUser} = useContext(AuthContext);
-  const {data} = useContext(ChatContext);
+  const ref = useRef()
+
+  useEffect(() => {
+    ref.current?.scrollIntoview({behavior: "smooth"})
+  },[message])
   return (
-    <div className='message owner'>
+    <div
+    ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
       <div className="message-info">
-        <img src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-1/335016940_900832347792588_7782826364201298440_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=aTiovSst5uAAX_GNqdw&_nc_ht=scontent.fsgn5-8.fna&oh=00_AfD5D1F7UOQlv_496O6LiIAAC-cqKG7iTsDZ3r00bLOHIA&oe=641F210C" alt="" />
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
         <span>Just now</span>
       </div>
       <div className="message-content">
-        <p>hello</p>
-        <img src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-1/335016940_900832347792588_7782826364201298440_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=aTiovSst5uAAX_GNqdw&_nc_ht=scontent.fsgn5-8.fna&oh=00_AfD5D1F7UOQlv_496O6LiIAAC-cqKG7iTsDZ3r00bLOHIA&oe=641F210C" alt="" />
+        <p>{message.text}</p>
+        <img
+          src={message.img}
+          alt=""
+        />
+        {message.img && 
+        <img
+          src={message.img}
+          alt=""
+        />
+        }
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
